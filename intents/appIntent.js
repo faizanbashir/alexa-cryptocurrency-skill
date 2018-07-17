6ae6ec09a64574424db61270cf595dc48281f23b
faizanbashir <faizan-bashir@live.com>
2018-07-18 01:04:35 +0530

    Updated error handling and response

--- intents/appIntent.js
+++ intents/appIntent.js
@@ -1,6 +1,7 @@
 const config = require('../config.js');
 
 const rp = require('request-promise');
+const fs = require('fs');
 
 const appIntent = {
     'LaunchRequest': function () {
@@ -11,9 +12,10 @@ const appIntent = {
     },
 
     'GetCryptoPriceIntent': function(){
-    	const cryptocurrency = this.event.request.intent.slots.cryptocurrency.value;
-
-        let RESPONSE_TEXT = "You have asked for the prices of "+ cryptocurrency;
+        const cryptocurrency = this.event.request.intent.slots.cryptocurrency.value;
+        let RESPONSE_TEXT = '';
+        let cryptoData = [];
+        let currency = '';
         
         if (!cryptocurrency) {
             RESPONSE_TEXT = "You need to provide the crypto currency name. Please try again!";
@@ -22,33 +24,43 @@ const appIntent = {
             this.emit(':responseReady');
         }
 
-        rp(config.COINMARKET_V2_API)
+        try {
+            cryptoData = JSON.parse(fs.readFileSync('cryptocurrency.json'));
+            currency = cryptoData[cryptocurrency];
+        } catch (e) {
+            RESPONSE_TEXT = "Sorry we were not able to find " + cryptocurrency + ". Can you please say that again?";
+            this.response.speak(RESPONSE_TEXT);
+            this.response.listen(config.HELP_REPROMPT);
+            this.emit(':responseReady');
+            console.log('Could not find currency: ' + cryptocurrency);
+        }
+
+        RESPONSE_TEXT = "You have asked for the prices of " + cryptocurrency;
+
+        rp(config.COINMARKET_V2_API + currency.id)
             .then(body => {
                 console.log(body);
                 
                 const result = JSON.parse(body);
-                let response_coin = null;
-                for(let  i = 0; i < result.length; i++){
-                    if(result[i].name.toLowerCase() == cryptocurrency.toLowerCase()){
-                        response_coin = result[i];
-                    }
-                }
 
-                if (!response_coin) {
-                    RESPONSE_TEXT = "Sorry we were not able to find your coin. Can you please say that again?";
+                if (result.metadata.error) {
+                    RESPONSE_TEXT = "There was an error processsing your request. Can you please say that again?";
                     this.response.speak(RESPONSE_TEXT);
                     this.response.listen(config.HELP_REPROMPT);
                     this.emit(':responseReady');
                 }
 
-                RESPONSE_TEXT = "The current price of "+ cryptocurrency + " is $"+ response_coin.price_usd + " per "+ cryptocurrency;
+                let price = result.data.quotes.USD.price;
+                console.log(result.data.quotes.USD.price);
+
+                RESPONSE_TEXT = "The current price of "+ cryptocurrency + " is $"+ price;
 
                 this.response.cardRenderer(RESPONSE_TEXT);
                 this.response.speak(RESPONSE_TEXT);
                 this.emit(':responseReady');
             })
             .catch(err => {
-                console.log("There was an error  during the request. Please try again! "+ err);
+                console.log("There was an error processsing your request. Please try again! " + err);
                 this.response.cardRenderer(RESPONSE_TEXT);
                 this.response.speak(RESPONSE_TEXT);
                 this.emit(':responseReady');
